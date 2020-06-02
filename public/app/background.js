86e5ea88c4bc20d9487103454cb6eb08c3146da7
David Calvo <dcalvo2@jhu.edu>
2020-06-01 20:55:49 -0400

    Hyperlink context menu

--- public/app/background.js
+++ public/app/background.js
@@ -1,6 +1,28 @@
+// List of file extensions we create context menu options on
+var fileExtensions = [
+    ".pdf",
+    ".rtf",
+    ".doc",
+    ".docx",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+    ".txt",
+    ".odt",
+    ".ods",
+    ".odp",
+    ".jpg",
+    ".jpeg",
+    ".gif",
+    ".png",
+    ".tiff",
+    ".tif",
+    ".bmp",
+];
 // List of MIME types we intercept downloads on
 // List of MIME types at https://www.iana.org/assignments/media-types/media-types.xhtml
-var fileExtensions = [
+var mimeTypes = [
     "application/pdf",
     "application/rtf",
     "application/msword",
@@ -21,17 +43,26 @@ var fileExtensions = [
 ];
 // Intercept downloads and ask to Scribe it
 chrome.downloads.onCreated.addListener(function (item) {
-    if (fileExtensions.includes(item.mime)) {
+    if (mimeTypes.includes(item.mime)) {
         chrome.downloads.cancel(item.id);
         alert(item.mime);
     }
 });
+function matchedUrlListBuilder() {
+    var matchedUrls = [];
+    for (var _i = 0, fileExtensions_1 = fileExtensions; _i < fileExtensions_1.length; _i++) {
+        var extension = fileExtensions_1[_i];
+        matchedUrls.push("*://*/*" + extension);
+    }
+    return matchedUrls;
+}
 // Create context menu option for images
 chrome.contextMenus.removeAll();
 chrome.contextMenus.create({
     title: "Convert with Scribe",
-    contexts: ["image"],
+    contexts: ["image", "link"],
     onclick: function () {
         alert('you\'ve been scribed');
-    }
+    },
+    targetUrlPatterns: matchedUrlListBuilder()
 });
--- public/app/background.ts
+++ public/app/background.ts
@@ -1,6 +1,29 @@
+// List of file extensions we create context menu options on
+const fileExtensions: Array<string> = [
+  ".pdf",
+  ".rtf",
+  ".doc",
+  ".docx",
+  ".xls",
+  ".xlsx",
+  ".ppt",
+  ".pptx",
+  ".txt",
+  ".odt",
+  ".ods",
+  ".odp",
+  ".jpg",
+  ".jpeg",
+  ".gif",
+  ".png",
+  ".tiff",
+  ".tif",
+  ".bmp",
+]
+
 // List of MIME types we intercept downloads on
 // List of MIME types at https://www.iana.org/assignments/media-types/media-types.xhtml
-const fileExtensions: Array<string> = [
+const mimeTypes: Array<string> = [
   "application/pdf",                                                              // .pdf
   "application/rtf",                                                              // .rtf
   "application/msword",                                                           // .doc
@@ -18,22 +41,33 @@ const fileExtensions: Array<string> = [
   "image/png",                                                                    // .png
   "image/tiff",                                                                   // .tif, .tiff
   "image/bmp",                                                                    // .bmp
-];
+]
 
 // Intercept downloads and ask to Scribe it
 chrome.downloads.onCreated.addListener(function(item) {
-  if (fileExtensions.includes(item.mime)){
-    chrome.downloads.cancel(item.id);
-    alert(item.mime);
+  if (mimeTypes.includes(item.mime)){
+    chrome.downloads.cancel(item.id)
+    alert(item.mime)
   }
-});
+})
+
+// Helper function to build match patterns since Chrome API doesn't support regex
+function matchedUrlListBuilder() {
+  let matchedUrls: Array<string> = []
+  for (let extension of fileExtensions) {
+    matchedUrls.push("*://*/*" + extension)
+  }
+
+  return matchedUrls
+}
 
 // Create context menu option for images
-chrome.contextMenus.removeAll();
+chrome.contextMenus.removeAll()
 chrome.contextMenus.create({
       title: "Convert with Scribe",
-      contexts: ["image"],
+      contexts: ["image", "link", "page_action"],
       onclick: function() {
-        alert('you\'ve been scribed');
-      }
-});
+        alert('you\'ve been scribed')
+      },
+      targetUrlPatterns: matchedUrlListBuilder()
+})
