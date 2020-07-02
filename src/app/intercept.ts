154b7bc6c19b1e3c188fed1c1d6c435c5192846a
David Calvo <dcalvo2@jhu.edu>
2020-07-02 19:54:17 -0400

    feat: Add hotkeys to download intercept window

--- src/app/intercept.ts
+++ src/app/intercept.ts
@@ -1,16 +1,15 @@
 let downloadObj: any = null // actually a JSON object containing info about the downloaded file from the background script
-let continueDownloadOnExit: boolean = true // used to decide if we auto-continue the download on window exit
 
 // window initialization that builds our download JSON object
 if (window.location.hash) {
     const downloadInfo = JSON.parse(atob(window.location.hash.substring(1))) // we remove the # and use atob to decode base64
-    const downloadName = downloadInfo.downloadURL.substring(downloadInfo.downloadURL.lastIndexOf('/') + 1).replace(/[\#\?].*$/,'')
+    const downloadName = downloadInfo.downloadURL.substring(downloadInfo.downloadURL.lastIndexOf('/') + 1).replace(/[\#\?].*$/, '')
 
     downloadObj = JSON.parse(`{"downloadURL":"${downloadInfo.downloadURL}", "downloadName":"${downloadName}", "downloadID":"${downloadInfo.downloadID}"}`)
 
     // logging to be removed
     console.log(downloadObj)
-    
+
 } else {
     console.log("window.location.hash error")
 }
@@ -19,19 +18,20 @@ if (window.location.hash) {
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("convert-with-scribe")?.addEventListener("click", convertWithScribe)
     document.getElementById("continue-download")?.addEventListener("click", continueDownload)
-    document.getElementById("downloadUrl")!.innerHTML = downloadObj.downloadURL
-    document.getElementById("downloadName")!.innerHTML = downloadObj.downloadName
+    document.getElementById("downloadName")!.innerHTML = `Would you like to convert or download "${downloadObj.downloadName}" from`
+    document.getElementById("downloadUrl")!.innerHTML = `${downloadObj.downloadURL}`
 })
 
 // handler for Escape-key exiting
 document.addEventListener('keydown', function (event) {
-    if (event.key === "Escape") {
-        if (downloadObj != null) {
-            continueDownload()
-        }
+    if (event.key == "Enter") {
+        convertWithScribe()
+    } else if (event.key == "Escape") {
+        continueDownload()
     }
 })
 
+let continueDownloadOnExit: boolean = true // used to decide if we auto-continue the download on window exit
 // handler used to ensure we always continue the user's download by default on window exit
 window.addEventListener('beforeunload', function (event) {
     if (continueDownloadOnExit) {
@@ -46,7 +46,9 @@ function convertWithScribe() {
 }
 
 function continueDownload() {
-    chrome.runtime.sendMessage({ action: "download", downloadURL: downloadObj.downloadURL, downloadName: downloadObj.downloadName, downloadID: downloadObj.downloadID })
+    if (downloadObj != null) {
+        chrome.runtime.sendMessage({ action: "download", downloadURL: downloadObj.downloadURL, downloadName: downloadObj.downloadName, downloadID: downloadObj.downloadID })
+    }
     continueDownloadOnExit = false // required otherwise we fall into an infinite loop
     window.close()
 }
